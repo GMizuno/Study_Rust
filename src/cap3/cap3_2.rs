8852867c798e15e5c15cc81db08f84560146c92b
Gabriel Mizuno <gabrielmizuno@gmail.com>
2024-03-24 11:19:56 -0300

    Controle de fluxo com While, Loop e For cap3

--- src/cap3/cap3_2.rs
+++ src/cap3/cap3_2.rs
@@ -1,3 +1,26 @@
 fn main() {
+    let mut counter = 0;
+    loop {
+        counter += 1;
+        println!("The counter is now: {counter}");
+        if counter == 5 {
+            break;
+        }
+    }
 
+    let mut counter = 0;
+    while counter < 5 {
+        counter += 1;
+        println!("The new counter is now: {counter}");
+    }
+
+    for number in 0..3 {
+        println!("The number is : {number}")
+    }
+
+    println!("New loop");
+
+    for number in 0..=3 {
+        println!("The number is : {number}")
+    }
 }
