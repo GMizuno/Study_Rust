67eb11d673e4cdda47b55c25b1261d50564812bb
Gabriel Mizuno <gabrielmizuno@gmail.com>
2024-05-08 21:34:49 -0300

    Mais exemplos sobre Cow

--- src/cap11/cap11_2.rs
+++ src/cap11/cap11_2.rs
@@ -30,6 +30,28 @@ struct User2<'a> {
     name: Cow<'a, str>,
 }
 
+// Exemplo 3 -> Concatenando &str ou String
+
+fn concat_strings<'a, 'b>(a: Cow<'a, str>, b: Cow<'b, str>) -> String {
+    let mut result = String::new();
+    result.push_str(a.as_ref());
+    result.push_str(b.as_ref());
+    result
+}
+
+// Exemplo 3.1 -> Complicando um pouco mais
+
+fn concat_strings2<'a, 'b>(a: &NewString<'a>, b: &NewString<'b>) -> String {
+    let mut result = String::new();
+    result.push_str(a.string.as_ref());
+    result.push_str(b.string.as_ref());
+    result
+}
+
+struct NewString<'a> {
+    string: Cow<'a, str>,
+}
+
 fn main() {
     println!("EXEMPLO 1");
 
@@ -96,4 +118,28 @@ fn main() {
             }
         }
     }
+
+    println!("EXEMPLO 3");
+
+    let str1 = String::from("Hello, ");
+    let str2 = "world!".to_owned();
+
+    let result1 = concat_strings(Cow::Borrowed(&str1), Cow::Borrowed(&str2)); // Passando referências
+    let result2 = concat_strings(Cow::Owned(str1), Cow::Owned(str2)); // Passando propriedades próprias
+
+    println!("Result 1: {}", result1);
+    println!("Result 2: {}", result2);
+
+    println!("EXEMPLO 3.1");
+
+    let str1_new = NewString {
+        string: Cow::Borrowed("Hello, "),
+    };
+    let str2_new = NewString {
+        string: Cow::Borrowed("world!"),
+    };
+
+    let result3 = concat_strings2(&str1_new, &str2_new);
+
+    println!("Result 3: {}", result3);
 }
