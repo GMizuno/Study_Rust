d19bff77cf56ff4b5db05f8548b76c03d0f14c16
Gabriel Mizuno <gabrielmizuno@gmail.com>
2024-05-14 21:22:57 -0300

    Estudando sobre threads

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,30 @@
-use std::sync::RwLock;
-
 fn main() {
-    let my_rwlock = RwLock::new(5);
-    let read1 = my_rwlock.read().unwrap();
-    let read2 = my_rwlock.read().unwrap();
-    println!("{read1:?}, {read2:?}");
+    let mut join_handles = vec![];
+    for num in 0..10 {
+        let handle = std::thread::spawn(|| {
+            println!("I am printing something");
+        });
+        join_handles.push(handle);
+    }
+    for handle in join_handles {
+        handle.join().unwrap();
+    }
 
-    // let write1 = my_rwlock.write().unwrap(); // deadlock
+    // let mut busy_work = vec![];
+    // // loop para segurar thread main
+    // for _ in 0..1_000_000 {
+    //     busy_work.push(9);
+    //     busy_work.pop();
+    // }
 
-    drop(read1);
-    drop(read2);
-
-    let mut write1 = my_rwlock.write().unwrap();
-    *write1 = 6;
-    println!("{:?}", my_rwlock);
-
-    println!("\n\nEXEMPLO COM TRY_LOCK\n\n");
-
-    let my_rwlock = RwLock::new(5);
-    let read1 = my_rwlock.read().unwrap();
-    let read2 = my_rwlock.read().unwrap();
-    println!("{read1:?}, {read2:?}");
-
-    let mut write1 = my_rwlock.try_write();
-
-    match write1 {
-        Ok(mut value) => {
-            *value += 10;
-            println!("Value {value}")
-        }
-        Err(_) => println!("Couldn't get write access, sorry"),
-    };
-
-    println!("\n\nEXEMPLO 2 COM TRY_LOCK\n\n");
-
-    let my_rwlock2 = RwLock::new(5);
-    let mut write2 = my_rwlock2.try_write();
-
-    println!("{write2:?}");
-
-    match write2 {
-        Ok(mut value) => {
-            println!("Value was {value}");
-            *value += 10;
-            println!("Now Value is {value}")
-        }
-        Err(_) => println!("Couldn't get write access, sorry"),
-    };
+    let mut join_handles = vec![];
+    for num in 0..10 {
+        let handle = std::thread::spawn(move || {
+            println!("Inside thread number: {num}");
+        });
+        join_handles.push(handle);
+    }
+    for handle in join_handles {
+        handle.join().unwrap();
+    }
 }
