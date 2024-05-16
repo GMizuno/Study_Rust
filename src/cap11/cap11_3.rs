cc63b536cc6619f1138e33c360b696fa3da025cf
Gabriel Mizuno <gabrielmizuno@gmail.com>
2024-05-15 21:26:57 -0300

    Avancando sobre Closures

--- src/cap11/cap11_3.rs
+++ src/cap11/cap11_3.rs
@@ -1,5 +1,30 @@
 fn main() {
-    std::thread::spawn(|| {
-        println!("I am printing something");
-    });
+    let mut join_handles = vec![];
+    for num in 0..10 {
+        let handle = std::thread::spawn(|| {
+            println!("I am printing something");
+        });
+        join_handles.push(handle);
+    }
+    for handle in join_handles {
+        handle.join().unwrap();
+    }
+
+    // let mut busy_work = vec![];
+    // // loop para segurar thread main
+    // for _ in 0..1_000_000 {
+    //     busy_work.push(9);
+    //     busy_work.pop();
+    // }
+
+    let mut join_handles = vec![];
+    for num in 0..10 {
+        let handle = std::thread::spawn(move || {
+            println!("Inside thread number: {num}");
+        });
+        join_handles.push(handle);
+    }
+    for handle in join_handles {
+        handle.join().unwrap();
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,8 @@
-fn main() {
-    let mut join_handles = vec![];
-    for num in 0..10 {
-        let handle = std::thread::spawn(|| {
-            println!("I am printing something");
-        });
-        join_handles.push(handle);
-    }
-    for handle in join_handles {
-        handle.join().unwrap();
-    }
-
-    // let mut busy_work = vec![];
-    // // loop para segurar thread main
-    // for _ in 0..1_000_000 {
-    //     busy_work.push(9);
-    //     busy_work.pop();
-    // }
-
-    let mut join_handles = vec![];
-    for num in 0..10 {
-        let handle = std::thread::spawn(move || {
-            println!("Inside thread number: {num}");
-        });
-        join_handles.push(handle);
-    }
-    for handle in join_handles {
-        handle.join().unwrap();
-    }
+fn do_something<F>(f: F)
+where
+    F: FnOnce(),
+{
+    f();
 }
+
+fn main() {}
